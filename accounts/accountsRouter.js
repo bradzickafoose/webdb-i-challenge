f28ddb0955680c6037a3f23c6a874df626a49932
Brad <brad@viziworx.com>
2020-05-24 11:43:37 -0400

    adds async/await and try/catch to get account by ID function

--- accounts/accountsRouter.js
+++ accounts/accountsRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const db = require('../data/dbConfig');
 const router = express.Router();
 
-
+// GET all accounts
 router.get('/', async (req, res) => {
   try {
     const accounts = await db('accounts');
@@ -12,19 +12,14 @@ router.get('/', async (req, res) => {
   }
 })
 
-router.get('/:id', (req, res) => {
-    db
-        .select('*')
-        .from('accounts')
-        .where({ id: req.params.id })
-        .first()
-        .then(response => {
-            res.status(200).json(response);
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).json(err)
-        })
+// GET Account by ID
+router.get('/:id', async (req, res) => {
+  try {
+    const account = await db('accounts').where({ id: req.params.id });
+    res.status(200).json(account);
+  } catch (error) {
+    res.status(500).json({ error: "Error retrieving account by ID", reason: error.message });
+  }
 })
 
 router.post('/', (req, res) => {
