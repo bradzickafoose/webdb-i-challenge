744c7efa02024056bcaac11fadf035b6aa1e7b64
Brad <brad@viziworx.com>
2020-05-24 13:36:48 -0400

    adds validateBody middleware and updates CREATE account function

--- accounts/accountsRouter.js
+++ accounts/accountsRouter.js
@@ -3,19 +3,15 @@ const db = require('../data/dbConfig');
 const router = express.Router();
 
 // CREATE account
-router.post('/', async (req, res) => {
+router.post('/', validateBody, async (req, res) => {
   const request = req.body;
 
   try {
-    if (!request.name || !request.budget) {
-      res.status(400).json({ error: "Please fill out all required fields" });
-    }
-    else {
-      const account = await db('accounts').insert(request);
-      res.status(201).json({ message: `The account for ${request.name} has been created`, id: account });
-    }
-  } catch (error) {
-      res.status(500).json({ error: "Error creating account", reason: error.message });
+    const account = await db('accounts').insert(request);
+    res.status(201).json({ message: `The account for ${request.name} has been created`, id: account });
+  }
+  catch (error) {
+    res.status(500).json({ error: "Error creating account", reason: error.message });
   }
 })
 
@@ -24,7 +20,8 @@ router.get('/', async (req, res) => {
   try {
     const accounts = await db('accounts');
     res.status(200).json(accounts);
-  } catch (error) {
+  }
+  catch (error) {
     res.status(500).json({ error: "Error retrieving accounts", reason: error.message });
   }
 })
@@ -34,17 +31,22 @@ router.get('/:id', async (req, res) => {
   try {
     const account = await db('accounts').where({ id: req.params.id });
     res.status(200).json(account);
-  } catch (error) {
+  }
+  catch (error) {
     res.status(500).json({ error: "Error retrieving account by ID", reason: error.message });
   }
 })
 
 // UPDATE account by ID
 router.put('/:id', async (req, res) => {
+  const id = Number(req.params.id);
+  const request = req.body;
+
   try {
-    const account = await db('accounts').where({ id: req.params.id }).update(req.body);
-    res.status(200).json({ message: "Account updated", id: account });
-  } catch (error) {
+    await db('accounts').where({ id: req.params.id }).update(request);
+    res.status(200).json({ message: `The account for ${request.name} has been updated`, id, request });
+  }
+  catch (error) {
     res.status(500).json({ message: "Error updating account", reason: error.message });
   }
 })
@@ -54,9 +56,24 @@ router.delete('/:id', async (req, res) => {
   try {
     const account = await db('accounts').where({ id: req.params.id }).del();
     res.status(200).json({ message: "Account deleted", id: account });
-  } catch (error) {
+  }
+  catch (error) {
     res.status(500).json({ message: "Error deleting account", reason: error.message });
   }
 })
+
+function validateBody(req, res, next) {
+  const { body } = req;
+
+  if (typeof body === undefined) {
+    res.status(400).json({ message: "Request is undefined" });
+  }
+  else if (!body.name || !body.budget) {
+    res.status(400).json({ error: "Please fill out all required fields" });
+  }
+  else {
+    next();
+  }
+}
 
 module.exports = router;
