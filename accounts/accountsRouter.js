3686fbbbf10568ff4e339aa827e9bf6af0888394
Brad <brad@viziworx.com>
2020-05-24 12:58:04 -0400

    Reorders CRUD functions. Adds new response message for CREATE account

--- accounts/accountsRouter.js
+++ accounts/accountsRouter.js
@@ -2,6 +2,23 @@ const express = require('express');
 const db = require('../data/dbConfig');
 const router = express.Router();
 
+// CREATE account
+router.post('/', async (req, res) => {
+  const accountData = req.body;
+
+  try {
+    if (!accountData.name || !accountData.budget) {
+      res.status(400).json({ error: "Please fill out all required fields" });
+    }
+    else {
+      const account = await db('accounts').insert(accountData);
+      res.status(201).json({ message: `The account for ${accountData.name} has been added`, id: account });
+    }
+  } catch (error) {
+      res.status(500).json({ error: "Error creating account", reason: error.message });
+  }
+})
+
 // READ all accounts
 router.get('/', async (req, res) => {
   try {
@@ -12,7 +29,7 @@ router.get('/', async (req, res) => {
   }
 })
 
-// READ Account by ID
+// READ account by ID
 router.get('/:id', async (req, res) => {
   try {
     const account = await db('accounts').where({ id: req.params.id });
@@ -22,40 +39,23 @@ router.get('/:id', async (req, res) => {
   }
 })
 
-// CREATE Account
-router.post('/', async (req, res) => {
-  const accountData = req.body;
-
-  try {
-    if (!accountData.name || !accountData.budget) {
-      res.status(400).json({ error: "Please fill out all required fields" });
-    }
-    else {
-      const account = await db('accounts').insert(accountData);
-      res.status(201).json({ message: "Account added", id: account });
-    }
-  } catch (error) {
-      res.status(500).json({ error: "Error creating account", reason: error.message });
-  }
-})
-
-// DELETE Account by ID
-router.delete('/:id', async (req, res) => {
-  try {
-    const account = await db('accounts').where({ id: req.params.id }).del();
-    res.status(200).json({ message: "Account deleted", id: account });
-  } catch (error) {
-    res.status(500).json({ message: "Error deleting account", reason: error.message });
-  }
-})
-
-// UPDATE Account by ID
+// UPDATE account by ID
 router.put('/:id', async (req, res) => {
   try {
     const account = await db('accounts').where({ id: req.params.id }).update(req.body);
     res.status(200).json({ message: "Account updated", id: account });
   } catch (error) {
     res.status(500).json({ message: "Error updating account", reason: error.message });
+  }
+})
+
+// DELETE account by ID
+router.delete('/:id', async (req, res) => {
+  try {
+    const account = await db('accounts').where({ id: req.params.id }).del();
+    res.status(200).json({ message: "Account deleted", id: account });
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting account", reason: error.message });
   }
 })
 
